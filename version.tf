a575f030b513c5dd985c561c8c6fd7243dbc88aa
Ruqayya Saheed-Baba <ruqayyarsb@gmail.com>
2026-02-03 20:03:15 -0500

    version.tf

--- version.tf
+++ version.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
         source = "harshicorp/aws"
-        version = "2.52.0.windows.1."
+        version = "2.52.0.windows.1"
     }
   }
 }
@@ -10,4 +10,3 @@ provider "aws" {
     region = "us-east-1"
 }
 
-#this is a comment
